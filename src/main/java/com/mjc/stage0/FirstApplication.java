8a85420d0763052a5055adbbece4d3c3be046f4d
Jhon <esteban0921@gmail.com>
2024-02-05 01:38:44 -0500

    MJC Hello world

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -1,7 +1,9 @@
 package com.mjc.stage0;
 
+
 public class FirstApplication {
     public static void main(String[] args) {
-        // Write your code here!
+        System.out.println("Hello, World!");
     }
 }
+
